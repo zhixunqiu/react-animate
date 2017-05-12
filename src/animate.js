95c70a4bf39617be62e5d8585fbd3527b2aa8662
wutaosusan <wutaosusan@didichuxing.com>
2017-05-12 19:00:17 +0800

    ll

--- src/animate.js
+++ src/animate.js
@@ -1,1 +1,25 @@
 
+import React, { Component,PropTypes } from 'react'
+import ReactDOM from 'react-dom'
+
+class Animate extends Component {
+  constructor(props) {
+    super(props)
+  }
+  static propTypes = {
+
+  }
+
+  static defaultProps = {
+
+  }
+
+  componentWillMount() {
+
+  }
+
+  render() {
+    return (<div>jjj</div>)
+  }
+}
+ export default Animate
